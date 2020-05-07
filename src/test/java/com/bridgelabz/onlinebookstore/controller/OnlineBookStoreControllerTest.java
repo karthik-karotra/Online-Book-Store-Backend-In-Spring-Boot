06c163828509baf607e47aea18bde5d3452ccc37
karthik-karotra <karthikpatel54@gmail.com>
2020-05-07 20:35:42 +0530

    ADDED:Negative Test Case Scenarious For Online Book Store Controller

--- src/test/java/com/bridgelabz/onlinebookstore/controller/OnlineBookStoreControllerTest.java
+++ src/test/java/com/bridgelabz/onlinebookstore/controller/OnlineBookStoreControllerTest.java
@@ -13,8 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,4 +41,35 @@ public class OnlineBookStoreControllerTest {
         MvcResult mvcResult = this.mockMvc.perform(get("/bookstore/list")).andReturn();
         Assert.assertTrue(mvcResult.getResponse().getContentAsString().contains(bookDetails.authorName));
     }
+
+    @Test
+    public void givenRequestToFetchListOfBooksFromDatabase_WhenGetResponse_ThenShouldReturnCorrectStatus() throws Exception {
+        bookDTO = new BookDTO(1000, "Mrutyunjay", "Shivaji Sawant", 400.0, 10, "Devotional", "bfjadlbfajlal", 2002);
+        bookDetails = new BookDetails(bookDTO);
+        List booksList = new ArrayList();
+        booksList.add(bookDetails);
+        when(onlineBookStoreService.getAllBooks()).thenReturn(booksList);
+        this.mockMvc.perform(get("/bookstore/list")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenRequestToFetchListOfBooksFromDatabase_WhenSendIncorrectUrlPath_ThenShouldReturnUrlNotFoundStatus() throws Exception {
+        bookDTO = new BookDTO(1000, "Mrutyunjay", "Shivaji Sawant", 400.0, 10, "Devotional", "bfjadlbfajlal", 2002);
+        bookDetails = new BookDetails(bookDTO);
+        List booksList = new ArrayList();
+        booksList.add(bookDetails);
+        this.mockMvc.perform(post("/index/list"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenRequestToFetchListOfBooksFromDatabase_WhenSendIncorrectRequestBody_ThenShouldReturnMethodNotAllowedStatus() throws Exception {
+        bookDTO = new BookDTO(1000, "Mrutyunjay", "Shivaji Sawant", 400.0, 10, "Devotional", "bfjadlbfajlal", 2002);
+        bookDetails = new BookDetails(bookDTO);
+        List booksList = new ArrayList();
+        booksList.add(bookDetails);
+        when(onlineBookStoreService.getAllBooks()).thenReturn(booksList);
+        this.mockMvc.perform(post("/bookstore/list")).andExpect(status().isMethodNotAllowed());
+    }
+
 }
