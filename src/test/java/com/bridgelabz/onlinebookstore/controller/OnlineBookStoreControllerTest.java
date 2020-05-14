baced1647a1ddbdcf2c22f7c2b206dc6ec121f59
abhishekvarmagh <abhishektheverma@gmail.com>
2020-05-15 02:59:15 +0530

    ADDED : Tested Controller API To Search & Sort At Same Time

--- src/test/java/com/bridgelabz/onlinebookstore/controller/OnlineBookStoreControllerTest.java
+++ src/test/java/com/bridgelabz/onlinebookstore/controller/OnlineBookStoreControllerTest.java
@@ -123,4 +123,16 @@ public class OnlineBookStoreControllerTest {
         MvcResult mvcResult=this.mockMvc.perform(get("/search/0/Mrutyunjay")).andReturn();
         Assert.assertTrue(mvcResult.getResponse().getContentAsString().contains("Mrutyunjay"));
     }
+
+    @Test
+    public void givenRequestToFilterBooksOnSearchedString_WhenFiltered_ShouldReturnBookDetails() throws Exception{
+        bookDTO = new BookDTO("1000", "Mrutyunjay", "Shivaji Sawant", 400.0, 10, "Devotional", "bfjadlbfajlal", 2002);
+        bookDetails = new BookDetails(bookDTO);
+        List bookList = new ArrayList();
+        bookList.add(bookDetails);
+        when(onlineBookStoreService.findAllBooks(any(),anyInt(),any())).thenReturn(bookList);
+        MvcResult mvcResult = this.mockMvc.perform(get("http://localhost:8080/sort/0/Mrutyunjay/LOW_TO_HIGH")).andReturn();
+        Assert.assertTrue(mvcResult.getResponse().getContentAsString().contains("1000"));
+    }
+
 }
